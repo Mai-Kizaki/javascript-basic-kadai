c7f33e0074e2c770056f7ef7b552c72a774c5b04
Mai-Kizaki <031424@gmail.com>
2026-02-06 17:49:22 +0900

    kadai_mdn_jsをアップロード

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,6 @@
+const date = new Date("2026-02-06");
+const year = date.getFullYear();
+const month = date.getMonth();
+const day = date.getDate();
+
+console.log(year + '年' + month + '月' + day + '日');
